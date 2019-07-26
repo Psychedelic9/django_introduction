bbf41f97e346f76a9d5619cc5480bc9696c676c2
Psychedelic9 <weeqqq@qq.com>
2019-07-26 17:08:11 +0800

    add

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Article(models.Model):
+    # ID
+    article_id = models.AutoField(primary_key=True)
+    # 作者
+    author = models.TextField()
+    # 标题
+    title = models.TextField()
+    # 摘要
+    brief_content = models.TextField()
+    # 主要内容
+    content = models.TextField()
+    # 发布日期
+    publish_date = models.DateTimeField(auto_now=True)
+
